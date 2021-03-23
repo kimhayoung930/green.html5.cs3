666bc28bfd32d678ef6052d8b72ccc54eb0526d6
kimhayoung930 <khy970930@naver.com>
2021-03-23 18:44:09 +0900

    0323

--- 자바스크립트연습/weather.js
+++ 자바스크립트연습/weather.js
@@ -7,44 +7,65 @@ var day1 = [], day2 = [] , day3 = [] ,day4 = [];
 var forecast = new Object();
 // temp
 forecast.tempShow = function(){
+    var str="";
     for(var i = 0; i<temp.length; i++){
-
+        str += temp[i] + "°C ";
     }
+        document.getElementById("show_temp").innerHTML = str + "°C ";
+        
 };
+
+
 forecast.tempAvgShow = function(){
     var num =0;
     for(var i = 0; i<temp.length; i++){
         num+= temp[i];
     }
     var avg = num / temp.length;
-    return avg;
+    document.getElementById("show_temp_avg").innerHTML = avg + "°C";
 };
+
 // rain
 forecast.rainShow = function(){
+    var str="";
     for(var i = 0; i<rain.length; i++){
+        str += rain[i] + "mm";
+}
+    document.getElementById("show_rain").innerHTML = str;
     
 };
-forecast.rainAvShow = function(){
+forecast.rainAvgShow = function(){
     var num =0;
     for(var i = 0; i<rain.length; i++){
         num+= rain[i];
     }
     var avg = num / rain.length;
-    return avg;
+    document.getElementById("show_rain_avg").innerHTML = avg + "mm";
 };
 // bust
 forecast.bustShow = function(){
+    var str="";
     for(var i = 0; i<bust.length; i++){
 
+        str += bust[i] + " ㎍/m³";
     }
+        document.getElementById("show_bust").innerHTML = str;
 };
+
 forecast.bustAvgShow = function(){
     var num =0;
     for(var i = 0; i<bust.length; i++){
         num+= bust[i];
     }
     var avg = num / bust.length;
-    return avg;
+    document.getElementById("show_bust_avg").innerHTML = avg + " ㎍/m³";
+
 };
 
+forecast.tempShow();
+forecast.rainShow();
+forecast.bustShow();
+forecast.tempAvgShow();
+forecast.rainAvgShow();
+forecast.bustAvgShow();
 
