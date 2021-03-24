47c7ce22fc2b2ab01f3781925bd0275b101337ac
kimhayoung930 <khy970930@naver.com>
2021-03-24 18:51:05 +0900

    0324

--- 자바스크립트연습/weather.js
+++ 자바스크립트연습/weather.js
@@ -4,68 +4,78 @@ var rain = [ 10, 50, 10 ,5];
 var bust = [40, 60, 70, 90];
 var day1 = [], day2 = [] , day3 = [] ,day4 = [];
 
-var forecast = new Object();
-// temp
-forecast.tempShow = function(){
-    var str="";
-    for(var i = 0; i<temp.length; i++){
-        str += temp[i] + "°C ";
-    }
-        document.getElementById("show_temp").innerHTML = str + "°C ";
-        
-};
+//var forecast = new Object();
 
-
-forecast.tempAvgShow = function(){
-    var num =0;
-    for(var i = 0; i<temp.length; i++){
-        num+= temp[i];
-    }
-    var avg = num / temp.length;
-    document.getElementById("show_temp_avg").innerHTML = avg + "°C";
-};
-
-// rain
-forecast.rainShow = function(){
-    var str="";
-    for(var i = 0; i<rain.length; i++){
-        str += rain[i] + "mm";
+function Forecast(arr){
+    this.foreCastArr = arr;
+switch(arr) {
+    case temp: this.division = "temp";
+        break;
+    case  rain:this.division = "rain";
+        break;
+    case  bust: this.division = "bust";
+        break;     
 }
-    document.getElementById("show_rain").innerHTML = str;
-    
-};
-forecast.rainAvgShow = function(){
-    var num =0;
-    for(var i = 0; i<rain.length; i++){
-        num+= rain[i];
-    }
-    var avg = num / rain.length;
-    document.getElementById("show_rain_avg").innerHTML = avg + "mm";
-};
-// bust
-forecast.bustShow = function(){
+ }
+// 보여주는 개념
+
+Forecast.prototype.show = function(){
     var str="";
-    for(var i = 0; i<bust.length; i++){
-
-        str += bust[i] + " ㎍/m³";
+    for(var i = 0; i<this.foreCastArr.length; i++){
+        str += this.foreCastArr[i] + this.unit() + " ";
     }
-        document.getElementById("show_bust").innerHTML = str;
+       return str;
 };
 
-forecast.bustAvgShow = function(){
+// 평균 구해주는 개념
+
+Forecast.prototype.avgShow= function(){
     var num =0;
-    for(var i = 0; i<bust.length; i++){
-        num+= bust[i];
+    for(var i = 0; i<this.foreCastArr.length; i++){
+        num+= this.foreCastArr[i];
     }
-    var avg = num / bust.length;
-    document.getElementById("show_bust_avg").innerHTML = avg + " ㎍/m³";
+ 
 
+    var avg = num / this.foreCastArr.length;
+
+    return avg.toFixed(1) + this.unit();
 };
 
-forecast.tempShow();
-forecast.rainShow();
-forecast.bustShow();
-forecast.tempAvgShow();
-forecast.rainAvgShow();
-forecast.bustAvgShow();
+//unit(단위를 붙혀주는 함수이다.)
+Forecast.prototype.unit = function(){
+    switch (this.division) {
+        case "temp": return " °C";
+            break;
 
+        case "rain": return "mm";
+            break;
+
+        case "bust": return "㎍/m³";
+            break;
+    }
+      
+  }
+
+  Forecast.prototype.innerHtml = function() {
+  var showId ="show_" + this.division;
+  var showAvgId ="show_" + this.division + "_avg";
+
+  
+
+  document.getElementById(showId).innerHtml = this.show();
+  document.getElementById(showAvgId).innerHtml = this.avgShow();
+   
+  }
+//함수호출
+var temp = new Forecast(temp);
+var rain = new Forecast(rain);
+var bust = new Forecast(bust);
+
+//Forecast (함수랑 객체들의 정보)
+temp.innerHtml();
+rain.innerHtml();
+bust.innerHtml();
+
+// Forecast.site(temp);
+// Forecast.site(rain);
+// Forecast.site(bust);
