59c2a0e6fd4746c21d9d570ff845ccb2342a80b6
kimhayoung930 <khy970930@naver.com>
2021-04-08 17:04:12 +0900

    0408

---
+++ assignment/과제3(21.04.11)/js/ottogi.js
@@ -0,0 +1,19 @@
+$(function(){
+    var trigger =$("#trigger");
+    var menu = $("nav #topMenu");
+
+    $(trigger).on("click", function(e){
+        e.preventDefault();
+        menu.slideToggle();
+           
+    });
+   
+    $(window).resize(function(){
+        var w=$(window).width();
+
+        if(w > 320 && menu.is(":hidden")){
+            menu.removeAttr("style");
+        }
+    });
+    //resize는 화면 크기에 따라 비율은 다르개해줌 
+});
